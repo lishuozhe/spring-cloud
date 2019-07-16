9c6d63b2137a179a5c5a5c8a826c8e3b859ccdb0
lisz <lisz@strongunion.com.cn>
2019-07-16 15:10:20 +0800

    新增zuul网关自定义拦截器和熔断

--- spring-cloud-zuul/src/main/java/cn/com/micro/zuul/SpringCloudZuulApplication.java
+++ spring-cloud-zuul/src/main/java/cn/com/micro/zuul/SpringCloudZuulApplication.java
@@ -3,6 +3,9 @@ package cn.com.micro.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import cn.com.micro.zuul.filter.TokenFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +13,10 @@ public class SpringCloudZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudZuulApplication.class, args);
+	}
+
+	@Bean
+	public TokenFilter tokenFilter() {
+		return new TokenFilter();
 	}
 }
