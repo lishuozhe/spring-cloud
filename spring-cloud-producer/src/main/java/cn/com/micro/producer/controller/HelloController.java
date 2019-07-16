ce13d65bd604f0a40d64d5137ed273d5f9452ed0
lisz <lisz@strongunion.com.cn>
2019-07-16 10:50:24 +0800

    新增消息总线更新配置

--- spring-cloud-producer/src/main/java/cn/com/micro/producer/controller/HelloController.java
+++ spring-cloud-producer/src/main/java/cn/com/micro/producer/controller/HelloController.java
@@ -1,11 +1,13 @@
 package cn.com.micro.producer.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
 
 	@Value("${say.hello}")
