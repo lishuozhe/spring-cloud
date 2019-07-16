80edc45cee124853167c73fdd7f578ecdc2527a6
1216535 <1216535@qq.com>
2019-07-16 14:03:12 +0800

    新增基础网关

---
+++ spring-cloud-zuul/src/main/java/cn/com/micro/zuul/SpringCloudZuulApplication.java
@@ -0,0 +1,14 @@
+package cn.com.micro.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringCloudZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZuulApplication.class, args);
+	}
+}
