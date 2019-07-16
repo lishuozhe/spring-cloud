2c6b835c2e5844f13dab38f23b732fedaa5cae51
lisz <lisz@strongunion.com.cn>
2019-07-16 16:17:45 +0800

    新增链路跟踪

--- README.md
+++ README.md
@@ -14,8 +14,10 @@ spring-cloud微服务架构
 spring-cloud
 ├── spring-cloud-config -- 配置中心（8200）
 ├── spring-cloud-eureka -- 注册中心（8000、8001、8002）
+├── spring-cloud-zuul -- 服务网关（8888）
 ├── spring-cloud-producer -- 服务提供（业务实现，按模块划分，如：用户、角色、权限）（9000）
 ├── spring-cloud-consumer -- 服务调用（业务实现，按业务划分，如：会员）（9100）
-└── spring-cloud-turbine -- 熔断监控（8100）
+├── spring-cloud-turbine -- 熔断监控（8100）
+└── spring-cloud-zipkin -- 链路跟踪（8300）
 
 ```
---
+++ spring-cloud-zipkin/src/main/java/cn/com/micro/zipkin/SpringCloudZipkinApplication.java
@@ -0,0 +1,18 @@
+package cn.com.micro.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class SpringCloudZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZipkinApplication.class, args);
+	}
+
+}
