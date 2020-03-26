b7dc628974246138491b1a4af8cae4a76aedfa44
lisz <lisz@strongunion.com.cn>
2020-03-26 18:54:00 +0800

    网关拦截认证接口 自动注入参数

--- spring-cloud-zuul/README.md
+++ spring-cloud-zuul/README.md
@@ -2,7 +2,9 @@
 spring-cloud微服务架构
 
 ### 服务网关
+```
 对外开放模块，路由到业务模块，认证模块不使用Swagger
+```
 
 - 接口调用: http://localhost:8888/admin/dict/1
 
--- spring-cloud-zuul/src/main/java/cn/com/lisz/zuul/SpringCloudZuulApplication.java
+++ spring-cloud-zuul/src/main/java/cn/com/lisz/zuul/SpringCloudZuulApplication.java
@@ -1,14 +1,22 @@
 package cn.com.lisz.zuul;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
 import com.spring4all.swagger.EnableSwagger2Doc;
 
 import springfox.documentation.swagger.web.SwaggerResource;
@@ -18,6 +26,24 @@ import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 @SpringBootApplication
 @EnableZuulProxy
 public class SpringCloudZuulApplication {
+
+	private final String GRANT_TYPE_PASSWORD = "password";
+
+	private final String GRANT_TYPE_PASSWORD_REFRESH_TOKEN = "refresh_token";
+
+	private final String CLIENT_ID_KEY = "client_id";
+
+	private final String CLIENT_SECRET_KEY = "client_secret";
+
+	private final String GRANT_TYPE_KEY = "grant_type";
+
+	private final String REFRESH_TOKEN_KEY = "refresh_token";
+
+	@Value("${auth.client_id}")
+	private String clientId;
+	
+	@Value("${auth.client_secret}")
+	private String clientSecret;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudZuulApplication.class, args);
@@ -42,4 +68,55 @@ public class SpringCloudZuulApplication {
 		}
 	}
 
+	@Component
+	public class ModifyHeaderFilter extends ZuulFilter {
+		@Override
+		public Object run() throws ZuulException {
+			String grantType = GRANT_TYPE_PASSWORD;
+			RequestContext requestContext = RequestContext.getCurrentContext();
+			HttpServletRequest request = requestContext.getRequest();
+			// 1、判断是刷新还是认证
+			String refreshToken = request.getParameter(REFRESH_TOKEN_KEY);
+			if (refreshToken != null) {
+				grantType = GRANT_TYPE_PASSWORD_REFRESH_TOKEN;
+			}
+			Map<String, List<String>> requestQueryParams = requestContext.getRequestQueryParams();
+			if (requestQueryParams == null)
+				requestQueryParams = new HashMap<>();
+			// 4、将要新增的参数添加进去,被调用的微服务可以直接去取,就想普通的一样,框架会直接注入进去
+			ArrayList<String> paramsList = new ArrayList<>();
+			paramsList.add(grantType);
+			requestQueryParams.put(GRANT_TYPE_KEY, paramsList);
+			paramsList = new ArrayList<>();
+			paramsList.add(clientId);
+			requestQueryParams.put(CLIENT_ID_KEY, paramsList);
+			paramsList = new ArrayList<>();
+			paramsList.add(clientSecret);
+			requestQueryParams.put(CLIENT_SECRET_KEY, paramsList);
+			requestContext.setRequestQueryParams(requestQueryParams);
+			return null;
+		}
+
+		@Override
+		public boolean shouldFilter() {
+			// 共享RequestContext，上下文对象
+			RequestContext requestContext = RequestContext.getCurrentContext();
+			HttpServletRequest request = requestContext.getRequest();
+			// 需要拦截的URL
+			if ("/security/oauth/token".equalsIgnoreCase(request.getRequestURI())) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public int filterOrder() {
+			return 0;
+		}
+
+		@Override
+		public String filterType() {
+			return "pre";
+		}
+	}
 }
